f56545c4e0d864da48477a967aab66c4361c3c30
BorisKourt <boriskourt@gmail.com>
2013-09-20 00:54:17 -0400

    Adjusted links

--- assets/js/ghostcat.site.js
+++ assets/js/ghostcat.site.js
@@ -1,3 +1,20 @@
 $(function(){
   mixpanel.track('Landed');
+
+  $('.tr-twitter').click(function(e){
+    mixpanel.track('Twitter');
+  });
+
+  $('.tr-repo').click(function(e){
+    mixpanel.track('Support');
+  });
+
+  $('.tr-ghostkick').click(function(e){
+    mixpanel.track('Ghost Kickstarter');
+  });
+
+  $('.tr_tryghost').click(function(e){
+    mixpanel.track('Try Ghost');
+  });
+  
 });
